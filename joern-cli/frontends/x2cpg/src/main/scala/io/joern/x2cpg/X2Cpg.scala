121b52694b3a7f201bf12beda64a0a053418b166
zeroy <zeroy0410@gmail.com>
2024-12-30 21:13:54 +0800

    up

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/X2Cpg.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/X2Cpg.scala
@@ -14,26 +14,31 @@ import java.nio.file.{Files, Paths}
 import scala.util.matching.Regex
 import scala.util.{Failure, Success, Try}
 
+// 定义默认配置
 object X2CpgConfig {
   def defaultInputPath: String  = ""
   def defaultOutputPath: String = "cpg.bin"
 }
 
+// 配置的基础特征，定义了输入输出路径、服务器模式等
 trait X2CpgConfig[R <: X2CpgConfig[R]] {
   var inputPath: String   = X2CpgConfig.defaultInputPath
   var outputPath: String  = X2CpgConfig.defaultOutputPath
   var serverMode: Boolean = false
 
+  // 设置输入路径
   def withInputPath(inputPath: String): R = {
     this.inputPath = Paths.get(inputPath).toAbsolutePath.normalize().toString
     this.asInstanceOf[R]
   }
 
+  // 设置输出路径
   def withOutputPath(x: String): R = {
     this.outputPath = x
     this.asInstanceOf[R]
   }
 
+  // 设置服务器模式
   def withServerMode(x: Boolean): R = {
     this.serverMode = x
     this.asInstanceOf[R]
@@ -43,21 +48,25 @@ trait X2CpgConfig[R <: X2CpgConfig[R]] {
   var ignoredFilesRegex: Regex             = "".r
   var ignoredFiles: Seq[String]            = Seq.empty
 
+  // 设置默认忽略文件的正则表达式
   def withDefaultIgnoredFilesRegex(x: Seq[Regex]): R = {
     this.defaultIgnoredFilesRegex = x
     this.asInstanceOf[R]
   }
 
+  // 设置忽略文件的正则表达式
   def withIgnoredFilesRegex(x: String): R = {
     this.ignoredFilesRegex = x.r
     this.asInstanceOf[R]
   }
 
+  // 设置忽略的文件
   def withIgnoredFiles(x: Seq[String]): R = {
     this.ignoredFiles = x.map(createPathForIgnore)
     this.asInstanceOf[R]
   }
 
+  // 创建用于忽略的文件路径
   def createPathForIgnore(ignore: String): String = {
     val path = Paths.get(ignore)
     if (path.isAbsolute) { path.toString }
@@ -66,6 +75,7 @@ trait X2CpgConfig[R <: X2CpgConfig[R]] {
 
   var schemaValidation: ValidationMode = ValidationMode.Disabled
 
+  // 设置模式验证
   def withSchemaValidation(value: ValidationMode): R = {
     this.schemaValidation = value
     this.asInstanceOf[R]
@@ -73,11 +83,13 @@ trait X2CpgConfig[R <: X2CpgConfig[R]] {
 
   var disableFileContent: Boolean = true
 
+  // 设置是否禁用文件内容
   def withDisableFileContent(value: Boolean): R = {
     this.disableFileContent = value
     this.asInstanceOf[R]
   }
 
+  // 继承字段配置
   def withInheritedFields(config: R): R = {
     this.inputPath = config.inputPath
     this.outputPath = config.outputPath
@@ -90,8 +102,7 @@ trait X2CpgConfig[R <: X2CpgConfig[R]] {
   }
 }
 
-/** Enables the configuration to specify if dependencies should be downloaded for additional symbol information.
-  */
+// 允许配置是否下载依赖项以获取额外的符号信息
 trait DependencyDownloadConfig[R <: X2CpgConfig[R]] { this: R =>
 
   def withDownloadDependencies(value: Boolean): R
@@ -102,7 +113,7 @@ object DependencyDownloadConfig {
   def parserOptions[R <: X2CpgConfig[R] & DependencyDownloadConfig[R]]: OParser[?, R] = {
     val builder = OParser.builder[R]
     import builder.*
-    OParser.sequence(
+    OParser.sequence( // 命令行解析库，当download-dependencies存在时，执行操作。
       opt[Unit]("download-dependencies")
         .text("Download the dependencies of the target project and use their symbols to resolve types.")
         .action((_, c) => c.withDownloadDependencies(true))
@@ -110,43 +121,33 @@ object DependencyDownloadConfig {
   }
 }
 
-/** Base class for `Main` classes of CPG frontends.
-  *
-  * Main classes that inherit from this base class parse the command line, exiting with an error code if this does not
-  * succeed. On success, the method `run` is called, which evaluates, given a frontend and a configuration, creates the
-  * CPG and stores it on disk.
-  *
-  * @param cmdLineParser
-  *   parser for command line arguments
-  * @param frontend
-  *   the frontend to use for CPG creation
-  */
+// CPG前端的Main类基类
 abstract class X2CpgMain[T <: X2CpgConfig[T], X <: X2CpgFrontend[T]](
-  val cmdLineParser: OParser[Unit, T],
-  val frontend: X
-)(implicit defaultConfig: T) {
+                                                                      val cmdLineParser: OParser[Unit, T],
+                                                                      val frontend: X
+                                                                    )(implicit defaultConfig: T) {
 
   private val logger = LoggerFactory.getLogger(classOf[X2CpgMain[T, X]])
 
+  // 记录版本和参数
   private def logVersionAndArgs(args: Array[String]): Unit = {
     val frontendName = frontend.getClass.getSimpleName.stripSuffix("$")
     val joernVersion =
-      // We only have a proper version there if joern was build using sbt assembly. Otherwise, it might be null.
+      // 只有在使用sbt assembly构建时才有适当的版本，否则可能为null
       Option(frontend.getClass.getPackage.getImplementationVersion).map(v => s"v$v").getOrElse("local build")
     val logText = s"Executing $frontendName ($joernVersion) with arguments: ${args.mkString(" ")}"
     logger.debug(logText)
   }
 
+  // 记录输出路径
   private def logOutputPath(outputPath: String): Unit = {
     if (X2CpgConfig.defaultOutputPath == outputPath) {
-      // We only log the output path of no explicit path was given by the user.
-      // Otherwise, the user obviously knows the path.
+      // 仅在用户未给定显式路径时记录输出路径
       logger.info(s"The resulting CPG will be stored at ${File(outputPath)}")
     }
   }
 
-  /** method that evaluates frontend with configuration
-    */
+  // 运行前端和配置的方法
   def run(config: T, frontend: X): Unit
 
   def main(args: Array[String]): Unit = {
@@ -166,44 +167,34 @@ abstract class X2CpgMain[T <: X2CpgConfig[T], X <: X2CpgFrontend[T]](
         System.exit(1)
     }
   }
-
 }
 
-/** Trait that represents a CPG generator, where T is the frontend configuration class.
-  */
+// 表示CPG生成器的特征，T是前端配置类
 trait X2CpgFrontend[T <: X2CpgConfig[T]] {
 
-  /** Create a CPG according to given configuration. Returns CPG wrapped in a `Try`, making it possible to detect and
-    * inspect exceptions in CPG generation. To be provided by the frontend.
-    */
+  // 根据给定配置创建CPG，返回Try以检测异常
   def createCpg(config: T): Try[Cpg]
 
-  /** Create CPG according to given configuration, printing errors to the console if they occur. The CPG is closed and
-    * not returned.
-    */
+  // 根据给定配置创建CPG，并在控制台打印错误（如果有）
   @throws[Throwable]("if createCpg throws any Throwable")
   def run(config: T): Unit = {
     withErrorsToConsole(config) { _ =>
       createCpg(config) match {
         case Success(cpg) =>
-          cpg.close() // persists to disk
+          cpg.close() // 持久化到磁盘
           Success(cpg)
         case Failure(exception) =>
           Failure(exception)
       }
     } match {
       case Failure(exception) =>
-        // We explicitly rethrow the exception so that every frontend will
-        // terminate with exit code 1 if there was an exception during createCpg.
-        // Frontend maintainer may want to catch that RuntimeException on their end
-        // to add custom error handling.
+        // 显式抛出异常以确保每个前端在createCpg中出现异常时终止
         throw exception
-      case Success(_) => // this is fine
+      case Success(_) => // 正常情况
     }
   }
 
-  /** Create a CPG with default overlays according to given configuration
-    */
+  // 根据给定配置创建带有默认覆盖层的CPG
   def createCpgWithOverlays(config: T): Try[Cpg] = {
     val maybeCpg = createCpg(config)
     maybeCpg.map { cpg =>
@@ -212,8 +203,7 @@ trait X2CpgFrontend[T <: X2CpgConfig[T]] {
     }
   }
 
-  /** Create a CPG for code at `inputPath` and apply default overlays.
-    */
+  // 为给定代码创建默认覆盖层的CPG
   def createCpgWithOverlays(inputName: String)(implicit defaultConfig: T): Try[Cpg] = {
     val maybeCpg = createCpg(inputName)
     maybeCpg.map { cpg =>
@@ -222,9 +212,7 @@ trait X2CpgFrontend[T <: X2CpgConfig[T]] {
     }
   }
 
-  /** Create a CPG for code at `inputName` (a single location) with default frontend configuration. If `outputName`
-    * exists, it is the file name of the resulting CPG. Otherwise, the CPG is held in memory.
-    */
+  // 为单个位置的代码创建CPG，带有默认前端配置
   def createCpg(inputName: String, outputName: Option[String])(implicit defaultConfig: T): Try[Cpg] = {
     val defaultWithInputPath = defaultConfig.withInputPath(inputName)
     val config = if (!outputName.contains(X2CpgConfig.defaultOutputPath)) {
@@ -239,8 +227,7 @@ trait X2CpgFrontend[T <: X2CpgConfig[T]] {
     createCpg(config)
   }
 
-  /** Create a CPG in memory for file at `inputName` with default configuration.
-    */
+  // 为文件创建内存中的CPG，使用默认配置
   def createCpg(inputName: String)(implicit defaultConfig: T): Try[Cpg] = createCpg(inputName, None)(defaultConfig)
 
 }
@@ -249,21 +236,17 @@ object X2Cpg {
 
   private val logger = LoggerFactory.getLogger(X2Cpg.getClass)
 
-  /** Parse commands line arguments in `args` using an X2Cpg command line parser, extended with the frontend specific
-    * options in `frontendSpecific` with the initial configuration set to `initialConf`. On success, the configuration
-    * is returned wrapped into an Option. On failure, error messages are printed and, `None` is returned.
-    */
+  // 使用X2Cpg命令行解析器解析命令行参数
   def parseCommandLine[R <: X2CpgConfig[R]](
-    args: Array[String],
-    frontendSpecific: OParser[?, R],
-    initialConf: R
-  ): Option[R] = {
+                                             args: Array[String],
+                                             frontendSpecific: OParser[?, R],
+                                             initialConf: R
+                                           ): Option[R] = {
     val parser = commandLineParser(frontendSpecific)
     OParser.parse(parser, args, initialConf)
   }
 
-  /** Create a command line parser that can be extended to add options specific for the frontend.
-    */
+  // 创建可扩展的命令行解析器
   private def commandLineParser[R <: X2CpgConfig[R]](frontendSpecific: OParser[?, R]): OParser[?, R] = {
     val builder = OParser.builder[R]
     import builder.*
@@ -278,10 +261,7 @@ object X2Cpg {
           c.withOutputPath(x)
         },
 
-      // previously this was supposed to be called with `,` as a separator,
-      // e.g. `--exclude foo,bar` - which (among others) has the disadvantage
-      // that under windows a `,` is treated as an argument separator
-      // better: provide this argument multiple times, i.e. `--exclude foo --exclude bar`
+      // 以前是用`,`作为分隔符，现在可以多次提供此参数
       opt[Seq[String]]("exclude")
         .valueName("<file1>")
         .unbounded()
@@ -317,8 +297,7 @@ object X2Cpg {
     )
   }
 
-  /** Create an empty CPG, backed by the file at `optionalOutputPath` or in-memory if `optionalOutputPath` is empty.
-    */
+  // 创建一个新的空CPG，可以在指定路径上存储或在内存中
   def newEmptyCpg(optionalOutputPath: Option[String] = None): Cpg = {
     optionalOutputPath match {
       case Some(outputPath) =>
@@ -332,9 +311,7 @@ object X2Cpg {
     }
   }
 
-  /** Apply function `applyPasses` to a newly created CPG. The CPG is wrapped in a `Try` and returned. On failure, the
-    * CPG is ensured to be closed.
-    */
+  // 对新创建的CPG应用函数applyPasses
   def withNewEmptyCpg[T <: X2CpgConfig[?]](outPath: String, config: T)(applyPasses: (Cpg, T) => Unit): Try[Cpg] = {
     val outputPath = if (outPath != "") Some(outPath) else None
     Try {
@@ -350,9 +327,7 @@ object X2Cpg {
     }
   }
 
-  /** Given a function that receives a configuration and returns an arbitrary result type wrapped in a `Try`, evaluate
-    * the function, printing errors to the console.
-    */
+  // 评估函数f并在控制台打印错误
   def withErrorsToConsole[T <: X2CpgConfig[?]](config: T)(f: T => Try[?]): Try[?] = {
     f(config) match {
       case Failure(exception) =>
@@ -363,8 +338,7 @@ object X2Cpg {
     }
   }
 
-  /** For a CPG generated by a frontend, run the default passes that turn a frontend-CPG into a complete CPG.
-    */
+  // 为前端生成的CPG运行默认的通过
   def applyDefaultOverlays(cpg: Cpg): Unit = {
     val context = new LayerCreatorContext(cpg)
     defaultOverlayCreators().foreach { creator =>
@@ -372,16 +346,12 @@ object X2Cpg {
     }
   }
 
-  /** This should be the only place where we define the list of default overlays.
-    */
+  // 定义默认覆盖层的唯一位置
   def defaultOverlayCreators(): List[LayerCreator] = {
     List(new Base(), new ControlFlow(), new TypeRelations(), new CallGraph())
   }
 
-  /** Write `sourceCode` to a temporary file inside a temporary directory. The prefix for the temporary directory is
-    * given by `tmpDirPrefix`. The suffix for the temporary file is given by `suffix`. Both file and directory are
-    * deleted on exit.
-    */
+  // 将sourceCode写入临时文件
   def writeCodeToFile(sourceCode: String, tmpDirPrefix: String, suffix: String): java.io.File = {
     val tmpDir = Files.createTempDirectory(tmpDirPrefix).toFile
     tmpDir.deleteOnExit()
@@ -391,12 +361,7 @@ object X2Cpg {
     tmpDir
   }
 
-  /** Strips surrounding quotation characters from a string.
-    * @param str
-    *   the target string.
-    * @return
-    *   the stripped string.
-    */
+  // 去除字符串的引号
   def stripQuotes(str: String): String = str.replaceAll("^(\"|')|(\"|')$", "")
 
 }
